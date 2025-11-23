0743fc939c4961c12028f74e1ad74f1eaa0e2eb8
SimoN <semeckinanton@gmail.com>
2025-11-23 15:20:56 +0300

    component "persons"

---
+++ src/app/pages/persons/persons.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-persons',
+  standalone: false,
+  templateUrl: './persons.html',
+  styleUrl: './persons.sass',
+})
+export class Persons {
+
+}
